fb7cbb5e78098401e1808eceb67a0554fe87beb2
Kepler9870u0987 <102182981+Kepler9870u0987@users.noreply.github.com>
2024-12-05 23:28:04 +0100

    creata app react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Benvenuto nel negozio di vino!</h1>
+      <p>Questo è un esempio di applicazione React.</p>
+    </div>
+  );
+}
+
+export default App;
